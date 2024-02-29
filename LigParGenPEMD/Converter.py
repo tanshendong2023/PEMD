7eef2191c788764503001d74a101f00543b5f9b6
tanshendong2023 <148203723+tanshendong2023@users.noreply.github.com>
2024-02-29 14:32:10 +0800

    1

--- LigParGenPEMD/Converter.py
+++ LigParGenPEMD/Converter.py
@@ -10,7 +10,6 @@ from openbabel import openbabel as ob
 obConversion = ob.OBConversion()
 obConversion.SetInAndOutFormats("pdb", "mol")
 
-
 def main():
 
     parser = argparse.ArgumentParser(
@@ -145,6 +144,7 @@ def convert(**kwargs):
     # assert (which('obabel')
     # is not None), "OpenBabel is Not installed or \n the executable location is not accessable"
     if os.path.exists(outdir + resname + '.xml'):
+        print('1')
         os.system('/bin/rm ' + outdir + resname + '.*')
     if lbcc:
         if charge == 0:
@@ -171,6 +171,7 @@ def convert(**kwargs):
         mol = BOSSReader('%s.z' % resname, '%s' % outdir, optim, charge, lbcc)
     elif pdb is not None:
         if not os.path.exists(os.path.join(outdir, pdb)):
+            print('no pdb file')
             os.system('cp %s %s' % (pdb, outdir))
         os.chdir(outdir)
         # Convert pdb to mol using Obabelv3
