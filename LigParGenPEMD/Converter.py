44a9e69819d8baf8975ec26872b7d55afaed711b
tanshendong2023 <148203723+tanshendong2023@users.noreply.github.com>
2024-02-29 14:23:11 +0800

    1

--- LigParGenPEMD/Converter.py
+++ LigParGenPEMD/Converter.py
@@ -145,7 +145,6 @@ def convert(**kwargs):
     # assert (which('obabel')
     # is not None), "OpenBabel is Not installed or \n the executable location is not accessable"
     if os.path.exists(outdir + resname + '.xml'):
-        print('yes')
         os.system('/bin/rm ' + outdir + resname + '.*')
     if lbcc:
         if charge == 0:
@@ -171,6 +170,9 @@ def convert(**kwargs):
         GenMolRep(mol, optim, resname, charge)
         mol = BOSSReader('%s.z' % resname, '%s' % outdir, optim, charge, lbcc)
     elif pdb is not None:
+        import os
+        current_path = os.getcwd()
+        print(current_path)
         if not os.path.exists(os.path.join(outdir, pdb)):
             os.system('cp %s %s' % (pdb, outdir))
         os.chdir(outdir)
