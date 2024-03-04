c8780e1a046910eb96c29cd9d09af79f0de464a8
tanshendong2023 <148203723+tanshendong2023@users.noreply.github.com>
2024-03-04 16:27:30 +0800

    1

--- PEMD/polymodel.py
+++ PEMD/polymodel.py
@@ -92,11 +92,12 @@ def build_oligomer(unit_name, repeating_unit, leftcap, rightcap, out_dir, Length
         Final_SMILES.append(smiles_each_ind)
 
         NumC = PEMD_lib.gen_conf_xyz_vasp(unit_name, m1, out_dir, ln, NumConf, NCores_opt, OPLS, )
-
+        print("hello")
         if NumC == 0 and ln == Length[-1]:
             return unit_name, 'FAILURE', Final_SMILES
         elif ln == Length[-1]:
             return unit_name, 'SUCCESS', Final_SMILES
+        print("hello")
     print("hello")
     # import os
     # # 保存当前工作目录的路径
