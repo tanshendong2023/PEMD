db8deaabecefa0677a43538260b2752842981b19
tanshendong2023 <148203723+tanshendong2023@users.noreply.github.com>
2024-05-14 23:32:01 +0800

    force field update

--- PEMD/analysis/prop.py
+++ PEMD/analysis/prop.py
@@ -11,8 +11,11 @@ import glob
 import subprocess
 import numpy as np
 import pandas as pd
-from importlib import resources
+import MDAnalysis as mda
+from tqdm.auto import tqdm
 from PEMD.model import PEMD_lib
+import matplotlib.pyplot as plt
+from importlib import resources
 from scipy.optimize import curve_fit
 
 
@@ -233,6 +236,314 @@ def fit_tg(df, param_file="fitting_tg.csv"):
     param_df.to_csv(param_file, index=False)
 
     return param_df
+
+
+def get_position(data_tpr_file, dcd_xtc_file, select_cations, select_anions, dt, dt_collection, run_start, nsteps,
+                 format='GROMACS'):
+
+    run = mda.Universe(data_tpr_file, dcd_xtc_file)
+
+    cations = run.select_atoms(select_cations).residues
+    anions = run.select_atoms(select_anions).residues
+
+    cations_list = cations.atoms.split("residue")
+    anions_list = anions.atoms.split("residue")
+
+    t_total = nsteps - run_start  # total simulation steps, minus equilibration time
+
+    times = None
+    if format == 'GROMACS':
+        times = np.arange(0, t_total * dt + 1, dt * dt_collection, dtype=int)
+    elif format == 'LAMMPS':
+        times = np.arange(0, t_total * dt, dt * dt_collection, dtype=int)
+
+    return run, cations, cations_list, anions, anions_list, times
+
+
+def create_position_arrays(run, cations_list, anions_list, times, run_start, dt_collection):
+
+    time = 0
+    cation_positions = np.zeros((len(times), len(cations_list), 3))
+    anion_positions = np.zeros((len(times), len(anions_list), 3))
+
+    for ts in enumerate(tqdm(run.trajectory[int(run_start/dt_collection):])):
+        system_com = run.atoms.center_of_mass(wrap=True)
+        for index, cation in enumerate(cations_list):
+            cation_positions[time, index, :] = cation.center_of_mass() - system_com
+        for index, anion in enumerate(anions_list):
+            anion_positions[time, index, :] = anion.center_of_mass() - system_com
+        time += 1
+
+    return cation_positions, anion_positions
+
+
+def autocorrFFT(x):
+
+    N = len(x)
+    F = np.fft.fft(x, n=2 * N)
+    PSD = F * F.conjugate()
+    res = np.fft.ifft(PSD)
+    res = (res[:N]).real
+    n = N * np.ones(N) - np.arange(0, N)
+    acf = res / n
+    return acf
+
+
+def msd_fft(r):
+
+    N = len(r)
+    D = np.square(r).sum(axis=1)
+    D = np.append(D, 0)
+    S2 = sum([autocorrFFT(r[:, i]) for i in range(r.shape[1])])
+    Q = 2 * D.sum()
+    S1 = np.zeros(N)
+    for m in range(N):
+        Q = Q - D[m - 1] - D[N - m]
+        S1[m] = Q / (N - m)
+    msd = S1 - 2 * S2
+    return msd
+
+
+def cross_corr(x, y):
+
+    N = len(x)
+    F1 = np.fft.fft(x, n=2 ** (N * 2 - 1).bit_length())
+    F2 = np.fft.fft(y, n=2 ** (N * 2 - 1).bit_length())
+    PSD = F1 * F2.conjugate()
+    res = np.fft.ifft(PSD)
+    res = (res[:N]).real
+    n = N * np.ones(N) - np.arange(0, N)
+    cf = res / n
+    return cf
+
+
+def msd_fft_cross(r, k):
+
+    N = len(r)
+    D = np.multiply(r, k).sum(axis=1)
+    D = np.append(D, 0)
+    S2 = sum([cross_corr(r[:, i], k[:, i]) for i in range(r.shape[1])])
+    S3 = sum([cross_corr(k[:, i], r[:, i]) for i in range(k.shape[1])])
+    Q = 2 * D.sum()
+    S1 = np.zeros(N)
+    for m in range(N):
+        Q = Q - D[m - 1] - D[N - m]
+        S1[m] = Q / (N - m)
+    msd = S1 - S2 - S3
+    return msd
+
+
+def calc_Lii_self(atom_positions, times):
+
+    Lii_self = np.zeros(len(times))
+    n_atoms = np.shape(atom_positions)[1]
+    for atom_num in (range(n_atoms)):
+        r = atom_positions[:,atom_num, :]
+        msd_temp = msd_fft(np.array(r))
+        Lii_self += msd_temp
+    msd = np.array(Lii_self)
+    return msd
+
+
+def calc_Lii(atom_positions, times):
+
+    r_sum = np.sum(atom_positions, axis = 1)
+    msd = msd_fft(r_sum)
+    return np.array(msd)
+
+
+def calc_Lij(cation_positions, anion_positions, times):
+
+    r_cat = np.sum(cation_positions, axis = 1)
+    r_an = np.sum(anion_positions, axis = 1)
+    msd = msd_fft_cross(np.array(r_cat),np.array(r_an))
+    return np.array(msd)
+
+
+def compute_all_Lij(cation_positions, anion_positions, times):
+
+    msd_self_cation = calc_Lii_self(cation_positions, times)
+    msd_self_anion =  calc_Lii_self(anion_positions, times)
+    msd_cation = calc_Lii(cation_positions, times)
+    msd_anion = calc_Lii(anion_positions, times)
+    msd_distinct_catAn = calc_Lij(cation_positions, anion_positions, times)
+    msds_all = [msd_cation, msd_self_cation, msd_anion, msd_self_anion, msd_distinct_catAn]
+    return msds_all
+
+
+def compute_slope_msd(msd, times, dt_collection, dt, interval_time):
+    log_time = np.log(times)
+    log_msd = np.log(msd)
+
+    dt_ = dt_collection * dt
+    interval_msd = int(interval_time / dt_)
+
+    closest_slope = float('inf')
+    time_range = (None, None)
+
+    for i in range(0, len(log_time) - interval_msd, interval_msd):
+        delta_y = log_msd[i + interval_msd] - log_msd[i]
+        delta_x = log_time[i + interval_msd] - log_time[i]
+        if delta_x != 0:
+            slope_log = delta_y / delta_x
+
+            if abs(slope_log - 1) < abs(closest_slope - 1):
+                closest_slope = slope_log
+                time_range = (times[i], times[i + interval_msd])
+
+    slope = (msd[int(time_range[1] / dt_)] - msd[int(time_range[0] / dt_)]) / (time_range[1] - time_range[0])
+    return slope, time_range
+
+
+def compute_self_diffusion(atom_positions, times, dt_collection, dt, interval_time):
+
+    n_atoms = np.shape(atom_positions)[1]
+    msd = calc_Lii_self(atom_positions, times) / n_atoms  # mean for particle
+
+    # Utilize the common slope calculation function
+    slope, time_range = compute_slope_msd(msd, times, dt_collection, dt, interval_time)
+
+    A2cm = 1e-8  # Angstroms to cm
+    ps2s = 1e-12  # picoseconds to seconds
+    convert = (A2cm ** 2) / ps2s   # cm^2/s
+    D = slope * convert / 6
+
+    return msd, D, time_range
+
+
+def plot_msd(msd, times, time_range, dt_collection, dt, label, x_range, y_range, save_file):
+
+    font_list = {"title": 20, "label": 18, "legend": 16, "ticket": 18, "data": 14}
+    color_list = ["#DF543F", "#2286A9", "#FBBF7C", "#3C3846"]
+
+    dt_ = dt_collection * dt
+
+    fig, ax = plt.subplots()
+
+    if len(msd) == 1:
+
+        mid_time = (time_range[1] - time_range[0]) / 2
+        start = int(10 ** (np.log10(mid_time) - 0.15) / dt_)
+        end = int(10 ** (np.log10(mid_time) + 0.15) / dt_)
+
+        scale = (msd[int(mid_time / dt_)] + 80) / mid_time
+
+        x_log = times[start:end]
+        y_log = x_log * scale
+
+        ax.plot(times[1:], msd[1:], '-', linewidth=1.5, color=color_list[0], label=label)
+        ax.plot(x_log, y_log, '--', linewidth=2, color="grey")
+
+    else:
+        mid_time = (time_range[0][1] - time_range[0][0]) / 2
+        start = int(10 ** (np.log10(mid_time) - 0.15) / dt_)
+        end = int(10 ** (np.log10(mid_time) + 0.15) / dt_)
+        scale = (msd[int(mid_time / dt_)] + 80) / mid_time
+
+        mid_time2 = (time_range[1][1] - time_range[1][0]) / 2
+        start2 = int(10 ** (np.log10(mid_time2) - 0.15) / dt_)
+        end2 = int(10 ** (np.log10(mid_time2) + 0.15) / dt_)
+        scale2 = (msd[int(mid_time2 / dt_)] + 80) / mid_time2
+
+        x_log = times[start:end]
+        y_log = x_log * scale
+
+        x_log2 = times[start2:end2]
+        y_log2 = x_log * scale2
+
+        ax.plot(times[0][1:], msd[0][1:], '-', linewidth=1.5, color=color_list[0], label=label)
+        ax.plot(times[1][1:], msd[1][1:], '-', linewidth=1.5, color=color_list[0], label=label)
+        ax.plot(x_log, y_log, '--', linewidth=2, color="grey")
+        ax.plot(x_log2, y_log2, '--', linewidth=2, color="grey")
+
+    ax.legend(fontsize=font_list["legend"], frameon=False)
+    ax.set_xlabel(r'$t$ (ps)', fontsize=font_list["label"])
+    ax.set_ylabel(r'MSD ($\AA^2$)', fontsize=font_list["label"])
+    ax.tick_params(axis='both', which='both', direction='in', labelsize=font_list["ticket"])
+
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlim(x_range[0], x_range[1])
+    ax.set_ylim(y_range[0], y_range[1])
+
+    ax.grid(True, linestyle='--')
+    fig.set_size_inches(5.5, 4)
+    plt.tight_layout()
+
+    # Save the plot
+    plt.savefig(f'{save_file}', bbox_inches='tight', dpi=300)
+    plt.show()
+
+
+def compute_conductivity(run, run_start, dt_collection, cations_list, anions_list, times, dt, interval_time):
+
+    # compute sum over all charges and positions
+    qr = []
+    for _ts in tqdm(run.trajectory[int(run_start / dt_collection):]):
+        qr_temp = np.zeros(3)
+        for cation in cations_list:
+            qr_temp += cation.center_of_mass() * int(1)
+        for anion in anions_list:
+            qr_temp += anion.center_of_mass() * int(-1)
+        qr.append(qr_temp)
+    msd = msd_fft(np.array(qr))
+
+    # Utilize the common slope calculation function
+    slope, time_range = compute_slope_msd(msd, times, dt_collection, dt, interval_time)
+
+    A2cm = 1e-8  # Angstroms to cm
+    ps2s = 1e-12  # picoseconds to seconds
+    e2c = 1.60217662e-19  # elementary charge to Coulomb
+    kb = 1.38064852e-23  # Boltzmann Constant, J/K
+    convert = e2c * e2c / ps2s / A2cm * 1000
+    T = 333  # K
+    v = (run.dimensions[0]) ** 3.0
+
+    cond = slope / 6 / kb / T / v * convert   # "mS/cm"
+
+    return msd, cond, time_range
+
+
+def compute_transfer_number(run, run_start, dt_collection, cation_positions, anion_positions, times, dt, interval_time):
+
+    A2cm = 1e-8  # Angstroms to cm
+    ps2s = 1e-12  # picoseconds to seconds
+    e2c = 1.60217662e-19  # elementary charge to Coulomb
+    kb = 1.38064852e-23  # Boltzmann Constant, J/K
+    convert = e2c * e2c / ps2s / A2cm * 1000
+    T = 333  # K
+    volume = (run.dimensions[0]) ** 3.0
+
+    msds_all = compute_all_Lij(cation_positions, anion_positions, times)
+
+    # Utilize the common slope calculation function
+    slope_plusplus = compute_slope_msd(msds_all[0], times, dt_collection, dt, interval_time)
+
+    slope_minusminus = compute_slope_msd(msds_all[2], times, dt_collection, dt, interval_time)
+
+    slope_plusminus = compute_slope_msd(msds_all[4], times, dt_collection, dt, interval_time)
+
+    A2cm = 1e-8  # Angstroms to cm
+    ps2s = 1e-12  # picoseconds to seconds
+    e2c = 1.60217662e-19  # elementary charge to Coulomb
+    kb = 1.38064852e-23  # Boltzmann Constant, J/K
+    convert = e2c * e2c / ps2s / A2cm * 1000
+    T = 333  # K
+    v = (run.dimensions[0]) ** 3.0
+
+    t = (slope_plusplus + slope_minusminus - 2 * slope_plusminus) / 6 / kb / T / v * convert
+
+    return t
+
+
+
+
+
+
+
+
+
+
 
 
 
